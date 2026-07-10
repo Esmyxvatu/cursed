2a34d6776d3e8e47ca5c0fa775a303cdb4005757
esmyxvatu <valentin.sicotpoulain@gmail.com>
2026-07-10 17:44:27 +0200

    feat: implemented the animation of falling and jumping

--- script/player.gd
+++ script/player.gd
@@ -9,23 +9,41 @@ const JUMP_VELOCITY = -400.0
 @onready var coat = $Coat
 @onready var sword = $Sword
 
+var jumping: bool = false
+var max_jump: float = -400
+var last_height: float = -42.0
+
 
 func _physics_process(delta: float) -> void:
 	# Show the animation
-	if velocity.x != 0:
-		feet.play("running")
+	if jumping:
+		coat.play("jump")
+		feet.play("jump")
+	elif not is_on_floor() and (coat.animation == "start_falling" or coat.animation == "falling"):
+		coat.play(coat.animation)
+		feet.play("falling")
+	elif velocity.x != 0:
 		coat.play("running")
+		feet.play("running")
 	else:
-		feet.play("idle")
 		coat.play("idle")
-	
+		feet.play("idle")
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if get_position_delta().y != 0:
+		last_height += get_position_delta().y
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jumping = true
+		max_jump = last_height - 75.0
+	
+	if Input.is_action_just_released("jump") or last_height <= max_jump:
+		jumping = false
+		coat.play("start_falling")
 
 	# Get the input direction and handle the movement/deceleration.
 	# should replace UI actions with custom gameplay actions.
@@ -35,6 +53,9 @@ func _physics_process(delta: float) -> void:
 		flip_sprites_around(direction == -1)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED / 4)
+
+	if jumping:
+		velocity.y = JUMP_VELOCITY
 
 	move_and_slide()
 
@@ -54,3 +75,8 @@ func flip_sprites_around(left: bool) -> void:
 		sword.flip_h = false
 		sword.flip_v = false
 		sword.rotation_degrees = 200
+
+
+func _on_coat_animation_looped() -> void:
+	if coat.animation == "start_falling":
+		coat.animation = "falling"
